a6ee4e78bfc688112e652adba4419ba70659d2ab
Vigneshwar M <40354785+vigneshwar221B@users.noreply.github.com>
2022-09-06 14:21:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
+
 $servername = "terraform-20220905111348513400000001.ckc51skzjcy0.us-east-1.rds.amazonaws.com";
 $username = "root";
-$password = "password";
+$password = trim(file_get_contents("/home/ubuntu/password.txt"));
+//$password="password";
 
 $hostname = gethostname();
 echo "<h1> from: $hostname </h1>";
 echo "<p>";
+echo "password: $password";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
